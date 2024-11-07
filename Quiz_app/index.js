a6af27198650be6c48a7c136969d7f9ef037b11c
aaanishaaa <anishaworks21@gmail.com>
2024-11-07 23:27:55 +0530

    more questions added

--- Quiz_app/index.js
+++ Quiz_app/index.js
@@ -2,7 +2,14 @@ const questions = [
     { question: "What is the capital of France?", choices: ["Berlin", "Madrid", "Paris", "Rome"], answer: "Paris" },
     { question: "The world’s nation 5G mobile network was launched by which country?", choices: ["Japan", "South Korea", "Singapore", "Malaysia"], answer: "South Korea" },
     { question: "Where was India’s first national Museum opened?", choices: ["Delhi", "Hyderabad", "Rajasthan", "Mumbai"], answer: "Mumbai" },
-    { question: "The green planet in the solar system is?", choices: ["Uranus", "Mars", "Jupiter", "Saturn"], answer: "Uranus" }
+    { question: "The green planet in the solar system is?", choices: ["Uranus", "Mars", "Jupiter", "Saturn"], answer: "Uranus" },
+    { question: "Which planet is known as the Red Planet?", choices: ["Earth", "Mars", "Jupiter", "Venus"], answer: "Mars" },
+    { question: "Who invented the light bulb?", choices: ["Albert Einstein", "Nikola Tesla", "Thomas Edison", "Alexander Graham Bell"], answer: "Thomas Edison" },
+    { question: "What is the smallest ocean in the world?", choices: ["Indian", "Arctic", "Atlantic", "Pacific"], answer: "Arctic" },
+    { question: "Which country is also known as the Land of the Rising Sun?", choices: ["China", "Japan", "Thailand", "South Korea"], answer: "Japan" },
+    { question: "What is the hardest natural substance on Earth?", choices: ["Gold", "Iron", "Diamond", "Platinum"], answer: "Diamond" },
+    { question: "Who wrote 'Hamlet'?", choices: ["William Wordsworth", "J.K. Rowling", "William Shakespeare", "Mark Twain"], answer: "William Shakespeare" },
+    { question: "What is the largest mammal on Earth?", choices: ["Elephant", "Blue Whale", "Giraffe", "Great White Shark"], answer: "Blue Whale" },
 ];
 
 let current_score = 0;
